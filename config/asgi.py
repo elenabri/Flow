5a6944920a17a79130322018f8c3ee5264a38831
elenabri <89944147+elenabri@users.noreply.github.com>
2026-04-23 16:34:57 +0300

    Update asgi.py

--- config/asgi.py
+++ config/asgi.py
@@ -1,13 +1,23 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
+
+# 1. Сначала устанавливаем настройки
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+# 2. Инициализируем Django
+django.setup()
+
+# 3. Инициализируем HTTP-приложение
+django_asgi_app = get_asgi_application()
+
+# 4. ТОЛЬКО ТЕПЕРЬ импортируем свои файлы (routing, consumers)
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import core.routing 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             core.routing.websocket_urlpatterns

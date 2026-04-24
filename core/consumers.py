eb26142b551f4b22253bac674178e2cad68a77ce
elenabri <89944147+elenabri@users.noreply.github.com>
2026-04-24 11:04:57 +0300

    Update consumers.py

--- core/consumers.py
+++ core/consumers.py
@@ -1,7 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model() # Это автоматически подтянет твой core.User
 from .models import Message
 
 class ChatConsumer(AsyncWebsocketConsumer):

25fba5c7932b79c2472e6ddd2ababbcb1731cacd
elenabri <89944147+elenabri@users.noreply.github.com>
2026-04-24 15:06:59 +0300

    Update context_processors.py

--- core/context_processors.py
+++ core/context_processors.py
@@ -2,7 +2,10 @@ from .models import Message
 
 def unread_messages_count(request):
     if request.user.is_authenticated:
-        # Считаем сообщения, где текущий пользователь — получатель и is_read = False
-        count = Message.objects.filter(receiver=request.user, is_read=False).count()
+        # Считаем сообщения в чатах пользователя, где отправитель не он сам
+        count = Message.objects.filter(
+            chat__participants=request.user,
+            is_read=False
+        ).exclude(sender=request.user).count()
         return {'unread_count': count}
-    return {'unread_count': 0} 
+    return {'unread_count': 0}

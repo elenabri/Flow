89018419fe373954a6797f59602e3e35635da3d6
elenabri <89944147+elenabri@users.noreply.github.com>
2026-04-24 15:12:27 +0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views  
-from .views import telegram_webhook
+from core.views import telegram_webhook
 
 
 # Группируем все маршруты основного приложения с именем 'core'

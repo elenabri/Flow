6f17f582446d192111cc9265c1dd01f54722c832
elenabri <89944147+elenabri@users.noreply.github.com>
2026-05-21 12:07:16 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -20,7 +20,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
-
+from .forms import CreativeInvoiceForm
 # Ваши модели и формы
 from .models import User, Chat, Message, BloggerProfile, AdvertiserProfile, ProductAd, AdContract, SupportTicket
 from .forms import RegistrationForm, EmailLoginForm

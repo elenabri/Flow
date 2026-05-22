f5696af430f652870c6c5aadc2f68a1ec96068eb
elenabri <89944147+elenabri@users.noreply.github.com>
2026-05-22 09:55:54 +0300

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1338,10 +1338,10 @@ class EridManagementView(View):
                         "contractor_role": "publisher",
                         "amount": {
                             "services": {
-                                "excluding_vat": str(excluding_vat),
-                                "vat_rate": "20",
-                                "vat": str(vat_val),
-                                "including_vat": str(amount_val)
+                                "excluding_vat": str(updated_creative.invoice_amount)
+                                "vat_rate": "0",
+                                "vat": "0.00",
+                                "including_vat": str(updated_creative.invoice_amount)
                             }
                         },
                         "items": [
@@ -1355,10 +1355,10 @@ class EridManagementView(View):
                                                 "shows_count": 0,
                                                 "invoice_shows_count": 0,
                                                 "amount": {
-                                                    "excluding_vat": str(excluding_vat),
-                                                    "vat_rate": "20",
+                                                    "excluding_vat": str(updated_creative.invoice_amount)
+                                                    "vat_rate": "0.00",
                                                     "vat": str(vat_val),
-                                                    "including_vat": str(amount_val)
+                                                    "including_vat": str(updated_creative.invoice_amount)
                                                 },
                                                 "pay_type": "cpm"
                                             }
